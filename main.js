9b7b003e2548b77386570fc99cc53252c25d76a5
Sara <sarailic431@gmail.com>
2024-10-27 13:51:56 +0100

    adding pictures and improving functionality

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 "use strict";
 import "./style.css";
+
+const btnLeft = document.querySelector(".btn-left");
+const btnRight = document.querySelector(".btn-right");
+const name = document.querySelector(".member");
+
+btnLeft.addEventListener("click", function () {
+  name.style.backgroundColor = "red";
+});
+
+btnRight.addEventListener("click", function () {
+  name.style.backgroundColor = "green";
+});
