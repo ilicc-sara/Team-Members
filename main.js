bc0eac01011c278d101a887ccdeac994abf76087
Sara <sarailic431@gmail.com>
2024-10-28 20:02:30 +0100

    modified JavaScript

--- main.js
+++ main.js
@@ -11,53 +11,54 @@ const text = document.querySelector(".info");
 
 const slides = [
   {
-    image: (pic.src = `/pic0.jpeg`),
-    title: (name.textContent = "Anna Smith"),
-    occupation: (job.textContent = `Designer`),
-    description:
-      (text.textContent = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio hic
+    image: `/pic0.jpeg`,
+    title: "Anna Smith",
+    occupation: `Designer`,
+    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio hic
         amet dolorem aliquam asperiores nesciunt doloremque blanditiis veritatis
         doloribus dolorum aperiam, id repudiandae expedita fuga animi nam nobis
-        sunt officiis. Lorem, ipsum dolor sit amet consectetur adipisicing elit.`),
+        sunt officiis. Lorem, ipsum dolor sit amet consectetur adipisicing elit.`,
   },
   {
-    image: (pic.src = `/pic1.jpeg`),
-    title: (name.textContent = "Michael Johnson"),
-    occupation: (job.textContent = `Manager`),
-    description:
-      (text.textContent = `Lorem dolor sit amet consectetur, adipisicing elit. Minus accusantium
+    image: `/pic1.jpeg`,
+    title: "Michael Johnson",
+    occupation: `Manager`,
+    description: `Lorem dolor sit amet consectetur, adipisicing elit. Minus accusantium
         ratione quia nam esse quaerat porro dolore libero fuga nesciunt dolores
         numquam voluptatem ipsum eveniet, quam, delectus necessitatibus pariatur
-        autem.`),
+        autem.`,
   },
   {
-    image: (pic.src = `/pic2.jpeg`),
-    title: (name.textContent = "Hailey Green"),
-    occupation: (job.textContent = `Customer Support`),
-    description:
-      (text.textContent = `Minus accusantium. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    image: `/pic2.jpeg`,
+    title: "Hailey Green",
+    occupation: `Customer Support`,
+    description: `Minus accusantium. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
         ratione quia nam esse quaerat porro dolore libero fuga nesciunt dolores
         numquam voluptatem ipsum eveniet, quam, delectus necessitatibus pariatur
-        autem.`),
+        autem.`,
   },
   {
-    image: (pic.src = `/pic3.jpeg`),
-    title: (name.textContent = "Matt Holland"),
-    occupation: (job.textContent = `Engineer`),
-    description: (text.textContent = `Distinctio hic
+    image: `/pic3.jpeg`,
+    title: "Matt Holland",
+    occupation: `Engineer`,
+    description: `Distinctio hic
         amet dolorem aliquam asperiores nesciunt doloremque blanditiis veritatis
         doloribus dolorum aperiam, id repudiandae expedita fuga animi nam nobis
         sunt officiis. Numquam voluptatem ipsum eveniet, quam, delectus necessitatibus pariatur
-        autem.`),
+        autem.`,
   },
 ];
 
+const updateScreen = function () {
+  pic.src = slides[activeIndex].image;
+  name.textContent = slides[activeIndex].title;
+  job.textContent = slides[activeIndex].occupation;
+  text.textContent = slides[activeIndex].description;
+};
+
 let activeIndex = 0;
 let maxSlide = slides.length;
-pic.src = slides[activeIndex].image;
-name.textContent = slides[activeIndex].title;
-job.textContent = slides[activeIndex].occupation;
-text.textContent = slides[activeIndex].description;
+updateScreen();
 
 // console.log(slides[3].image);
 console.log(slides.length);
@@ -66,22 +67,16 @@ btnLeft.addEventListener("click", function () {
   if (activeIndex === 0) {
     activeIndex = maxSlide - 1;
   } else {
-    --activeIndex;
+    activeIndex--;
   }
-  pic.src = slides[activeIndex].image;
-  name.textContent = slides[activeIndex].title;
-  job.textContent = slides[activeIndex].occupation;
-  text.textContent = slides[activeIndex].description;
+  updateScreen();
 });
 
 btnRight.addEventListener("click", function () {
   if (activeIndex === maxSlide - 1) {
     activeIndex = 0;
   } else {
-    ++activeIndex;
+    activeIndex++;
   }
-  pic.src = slides[activeIndex].image;
-  name.textContent = slides[activeIndex].title;
-  job.textContent = slides[activeIndex].occupation;
-  text.textContent = slides[activeIndex].description;
+  updateScreen();
 });
